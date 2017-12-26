d93b08f57b6b4496637d3080b97d84600a8a164f
stepan7 <fenixrnd@mail.ru>
2017-12-26 08:44:31 +0300

    first commit

---
+++ mysite/mysite/urls.py
@@ -0,0 +1,9 @@
+
+from django.conf.urls import url, include
+from django.contrib import admin
+
+urlpatterns = [
+    url(r'^admin/', admin.site.urls),
+    url(r'^web/', include('web.urls')),
+    url(r'^', include('main.urls')),
+]
